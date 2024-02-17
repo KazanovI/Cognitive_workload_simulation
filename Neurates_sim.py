fb0d7b4241f92cd67a220918492a8e356fadc2d8
KazanovI <itaygivotbar@gmail.com>
2024-02-17 19:13:27 +0200

    fixes

--- Neurates_sim.py
+++ Neurates_sim.py
@@ -1,55 +1,56 @@
 import numpy as np
 
-def simulate_session(sess,cwl,level,perf):
-    for protocol in range(level.shape[2]): # per protocol
+
+def simulate_session(sess, cwl, level, perf):
+    # add random noise that represents factors other than CWL
+    rand_noise = np.random.normal(-0.05, 0.1, N_CHARACTERS)  # same for both protocols in each session
+    for protocol in range(level.shape[2]):  # per protocol
         # adjust CWL based on previous levels
         if sess != 0:
             # change cognitive workload based on previous session
-            cwl = change_cwl(cwl,level,sess,protocol)
-            # get performance level from each player based on cwl
-            level_per = calc_performance(cwl[:, sess, protocol])
-            perf[:,sess, protocol] = level_per
-        else:
-            if protocol == 0: # on first day both protocols have the same properties
-                # get performance level from each player
-                level_per = calc_performance(cwl[:, sess, protocol])
-                perf[:, sess, protocol] = level_per
+            cwl = change_cwl(cwl, level, sess, protocol)
         # get performance level from each player
-        if protocol == 0: # default
-            # adjust level based on performance
-            level = change_level(level,level_per,sess,protocol)
-        elif protocol == 1: # neuro-adaptive
-            # adjust level based on performance
-            level = change_level(level,level_per, sess, protocol)
-            # adjust level based on CWL. this step can cause a player to jump 2 levels eventually, or alternatively
-            # cancel the rise in level from the default protocol
-            UP_BOUNDRY = [0.4, 0.6] # between values
-            DOWN_BOUNDRY = [0.1, 0.9] # above/below values
-            up_na = np.logical_and((cwl[:,sess,protocol] > UP_BOUNDRY[0]) , (cwl[:,sess,protocol] < UP_BOUNDRY[1]))
-            down_na = np.logical_or((cwl[:,sess,protocol] < DOWN_BOUNDRY[0]) , (cwl[:,sess,protocol] > DOWN_BOUNDRY[1]))
-            level[up_na, sess + 1, protocol] = level[up_na, sess + 1, protocol] + 1
-            level[down_na, sess + 1, protocol] = level[down_na, sess + 1, protocol] - 1
-            level[~up_na & ~down_na, sess + 1, protocol] = level[~up_na & ~down_na, sess + 1, protocol]
-            # check not exceeding level boundaries [1 20]
+        level_per = calc_performance(cwl[:, sess, protocol])
+        level_per = level_per + rand_noise
+        level_per_ad = adjust_perf(level_per)
+        perf[:, sess, protocol] = level_per_ad
+        # get performance level from each player and adjust next level
+        if sess < N_SESSIONS - 1:
+            if protocol == 0:  # default
+                # adjust level based on performance
+                level = change_level(level, level_per_ad, sess, protocol)
+            elif protocol == 1:  # neuro-adaptive
+                # adjust level based on performance
+                level = change_level(level, level_per_ad, sess, protocol)
+                # adjust level based on CWL. this step can cause a player to jump 2 levels eventually, or alternatively
+                # cancel the rise in level from the default protocol
+                UP_BOUNDRY = [0.45, 0.55]  # between values
+                DOWN_BOUNDRY = [0, 0.8]  # above/below values
+                up_na = np.logical_and((cwl[:, sess, protocol] > UP_BOUNDRY[0]),
+                                       (cwl[:, sess, protocol] < UP_BOUNDRY[1]))
+                down_na = np.logical_or((cwl[:, sess, protocol] < DOWN_BOUNDRY[0]),
+                                        (cwl[:, sess, protocol] > DOWN_BOUNDRY[1]))
+                level[up_na, sess + 1, protocol] = level[up_na, sess + 1, protocol] + 1
+                level[down_na, sess + 1, protocol] = level[down_na, sess + 1, protocol] - 1
+                level[~up_na & ~down_na, sess + 1, protocol] = level[~up_na & ~down_na, sess + 1, protocol]
+                # check not exceeding level boundaries [1 20]
             level[:, :, protocol] = adjust_level(level[:, :, protocol])
-    return cwl,level,perf # returns output of session
+    return cwl, level, perf  # returns output of session
+
 
 def calc_performance(cwl_per):
     # relation between performance and cwl = inverted U shape
     num_xpoints = 1000
-    xvals = np.arange(1,num_xpoints+1)
+    xvals = np.arange(1, num_xpoints + 1)
     # formula for inverted U shape relationship
-    cwl_perf_relation = ((-1/250)*(xvals-500)**2)+num_xpoints
+    cwl_perf_relation = ((-1 / 250) * (xvals - 500) ** 2) + num_xpoints
     # get current session cwl
     cwl_sess = cwl_per
-    indices = np.floor(cwl_sess*(num_xpoints-1)).astype('int')
+    indices = np.floor(cwl_sess * (num_xpoints - 1)).astype('int')
     # get performance based on cwl
-    perf_in_session = cwl_perf_relation[indices]/num_xpoints
-    # add random noise that represents factors other than CWL
-    perf_in_session = perf_in_session + (perf_in_session * np.random.normal(-0.05, 0.1, N_CHARACTERS))
-    # adjust performance to not pass limits [0 1]
-    perf_in_session_ad = adjust_perf(perf_in_session)
-    return perf_in_session_ad
+    perf_in_session = cwl_perf_relation[indices] / num_xpoints
+    return perf_in_session
+
 
 def adjust_perf(perf):
     fix_top = np.where(perf > 1)
@@ -60,18 +61,19 @@ def adjust_perf(perf):
         perf[fix_bottom] = 0
     return perf
 
+
 def adjust_level(level):
-    level_ad = level
     fix_top = np.where(level > N_LEVELS)
     if np.any(fix_top):
         level[fix_top] = N_LEVELS
     fix_bottom = np.where(level < 1)
     if np.any(fix_bottom):
         level[fix_bottom] = 1
-    return level_ad
+    return level
 
-def change_level(level_array,performance,sess,protocol):
-    PER_BOUNDRY = [0.55, 0.9] # change level below/above performance boundary
+
+def change_level(level_array, performance, sess, protocol):
+    PER_BOUNDRY = [0.55, 0.9]  # change level below/above performance boundary
     ind_down = performance <= PER_BOUNDRY[0]
     ind_up = performance >= PER_BOUNDRY[1]
     level_array[ind_up, sess + 1, protocol] = level_array[ind_up, sess, protocol] + 1
@@ -81,7 +83,8 @@ def change_level(level_array,performance,sess,protocol):
     level_array[:, :, protocol] = adjust_level(level_array[:, :, protocol])
     return level_array
 
-def change_cwl(cwl,level,sess,protocol):
+
+def change_cwl(cwl, level, sess, protocol):
     level_rep = 0.1
     level_down = 0.2
     level_up = 0.2
@@ -91,9 +94,9 @@ def change_cwl(cwl,level,sess,protocol):
     # no change(same level again) - change cwl toward 0.5
     no_change_ind = level[:, sess, protocol] == level[:, sess - 1, protocol]
     cwl[no_change_ind & too_easy_ind, sess, protocol] = cwl[no_change_ind & too_easy_ind, sess - 1, protocol] + (
-                cwl[no_change_ind & too_easy_ind, sess - 1, protocol] * level_rep)
+            cwl[no_change_ind & too_easy_ind, sess - 1, protocol] * level_rep)
     cwl[no_change_ind & too_hard_ind, sess, protocol] = cwl[no_change_ind & too_hard_ind, sess - 1, protocol] + (
-                cwl[no_change_ind & too_hard_ind, sess - 1, protocol] * -level_rep)
+            cwl[no_change_ind & too_hard_ind, sess - 1, protocol] * -level_rep)
     cwl[no_change_ind & ~too_easy_ind & ~too_hard_ind, sess, protocol] = cwl[
         no_change_ind & ~too_easy_ind & ~too_hard_ind, sess - 1, protocol]
     # level up - increase cwl
@@ -102,26 +105,28 @@ def change_cwl(cwl,level,sess,protocol):
     # level down - change cwl toward 0.5
     down_ind = level[:, sess, protocol] - level[:, sess - 1, protocol] <= -1
     cwl[down_ind & too_easy_ind, sess, protocol] = cwl[down_ind & too_easy_ind, sess - 1, protocol] + (
-                cwl[down_ind & too_easy_ind, sess - 1, protocol] * level_down)
+            cwl[down_ind & too_easy_ind, sess - 1, protocol] * level_down)
     cwl[down_ind & too_hard_ind, sess, protocol] = cwl[down_ind & too_hard_ind, sess - 1, protocol] + (
-                cwl[down_ind & too_hard_ind, sess - 1, protocol] * -level_down)
+            cwl[down_ind & too_hard_ind, sess - 1, protocol] * -level_down)
     cwl[down_ind & ~too_easy_ind & ~too_hard_ind, sess, protocol] = cwl[
         down_ind & ~too_easy_ind & ~too_hard_ind, sess - 1, protocol]
     cwl = adjust_perf(cwl)
     return cwl
 
-def run_sim(NUM_SESSIONS,NUM_LEVELS,NUM_CHARACTERS,CWL_LEVEL,PERFORMANCE_LEVEL,SIM_LEVEL):
-    global N_LEVELS,N_CHARACTERS
+
+def run_sim(NUM_SESSIONS, NUM_LEVELS, NUM_CHARACTERS, CWL_LEV, PERFORMANCE_LEV, SIM_LEV):
+    global N_SESSIONS, N_LEVELS, N_CHARACTERS
     N_LEVELS = NUM_LEVELS
     N_CHARACTERS = NUM_CHARACTERS
+    N_SESSIONS = NUM_SESSIONS
     for sess in range(NUM_SESSIONS):
         # pass the "players" properties and return data
-        cwl_next, level_next, perf_next = simulate_session(sess,CWL_LEVEL,SIM_LEVEL,PERFORMANCE_LEVEL)
-        CWL_LEVEL = cwl_next
-        SIM_LEVEL = level_next
-        PERFORMANCE_LEVEL = perf_next
-    return CWL_LEVEL,SIM_LEVEL,PERFORMANCE_LEVEL
+        cwl_next, level_next, perf_next = simulate_session(sess, CWL_LEV, SIM_LEV, PERFORMANCE_LEV)
+        CWL_LEV = cwl_next
+        SIM_LEV = level_next
+        PERFORMANCE_LEV = perf_next
+    return CWL_LEV, SIM_LEV, PERFORMANCE_LEV
+
 
 if __name__ == "__main__":
     print('exc code')
-
--- analyze_sim.py
+++ analyze_sim.py
@@ -2,34 +2,49 @@ import Neurates_sim
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import colors
+from scipy.stats import sem
 
-
-
-
-NUM_SESSIONS = 20 # how many sessions in simulation
-NUM_LEVELS = 10 # maximum level that can be reached
+NUM_SESSIONS = 20  # how many sessions in simulation
+NUM_LEVELS = 10  # maximum level that can be reached
 NUM_CHARACTERS = 100
-mu, sigma = 0.5, 0.1 # mean and standard deviation
-BASE_CWL = np.random.normal(mu, sigma, NUM_CHARACTERS) # baseline level of each character's cognitive working load
+mu, sigma = 0.5, 0.1  # mean and standard deviation
+BASE_CWL = np.random.normal(mu, sigma, NUM_CHARACTERS)  # baseline level of each character's cognitive working load
 BASE_CWL = Neurates_sim.adjust_perf(BASE_CWL)
-CWL_LEVEL = np.zeros((NUM_CHARACTERS,NUM_SESSIONS,2))
-CWL_LEVEL[:,0,:] = np.tile(BASE_CWL,(2,1)).T
-PERFORMANCE_LEVEL = np.array(np.zeros((NUM_CHARACTERS,NUM_SESSIONS,2)))
-SIM_LEVEL = np.array(np.zeros((NUM_CHARACTERS,NUM_SESSIONS+1,2))) # level reached in simulation
-SIM_LEVEL[:,0,:] = 1 # start from level 1
+n_iter = 100
+cwl_iter = np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2, n_iter))
+level_iter = np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2, n_iter))
+perf_iter = np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2, n_iter))
 # run simulation
-cwl,level,perf = Neurates_sim.run_sim(NUM_SESSIONS,NUM_LEVELS,NUM_CHARACTERS,CWL_LEVEL,PERFORMANCE_LEVEL,SIM_LEVEL)
+for it in range(n_iter):
+    CWL_LEVEL = np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2))
+    CWL_LEVEL[:, 0, :] = np.tile(BASE_CWL, (2, 1)).T
+    PERFORMANCE_LEVEL = np.array(np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2)))
+    SIM_LEVEL = np.array(np.zeros((NUM_CHARACTERS, NUM_SESSIONS, 2)))  # level reached in simulation
+    SIM_LEVEL[:, 0, :] = 1  # start from level 1
+    cwl, level, perf = Neurates_sim.run_sim(NUM_SESSIONS,
+                                            NUM_LEVELS,
+                                            NUM_CHARACTERS,
+                                            CWL_LEVEL,
+                                            PERFORMANCE_LEVEL,
+                                            SIM_LEVEL)
+    cwl_iter[:, :, :, it] = cwl
+    level_iter[:, :, :, it] = level
+    perf_iter[:, :, :, it] = perf
+# mean over iterations
+m_cwl = np.mean(cwl_iter, axis=3)
+m_level = np.mean(level_iter, axis=3)
+m_perf = np.mean(perf_iter, axis=3)
 
 # plots section
-def_progress = level[:,:,0]
-na_progress = level[:,:,1]
+def_progress = m_level[:, :, 0]
+na_progress = m_level[:, :, 1]
 
 # heatmap to look at all the players together, sorted by base cwl
 sort_ind = np.argsort(BASE_CWL)
-na_prog_sorted = na_progress[sort_ind,:]
-def_prog_sorted = def_progress[sort_ind,:]
+na_prog_sorted = na_progress[sort_ind, :]
+def_prog_sorted = def_progress[sort_ind, :]
 images = []
-fig, axs = plt.subplots(1,2)
+fig, axs = plt.subplots(1, 2)
 images.append(axs[0].imshow(na_prog_sorted))
 axs[0].set_title("Progression by NeuroAdaptive")
 axs[0].set_xlabel('Session')
@@ -44,35 +59,37 @@ vmax = max(image.get_array().max() for image in images)
 norm = colors.Normalize(vmin=vmin, vmax=vmax)
 for im in images:
     im.set_norm(norm)
-plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9, left = 0.1)
-fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1,label='Level')
-plt.show()
+plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9, left=0.1)
+fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1, label='Level')
+# plt.show()
 # look at the difference
+f, (ax, cax) = plt.subplots(2, 1, figsize=(6, 4), gridspec_kw={"height_ratios": [1, 0.05]})
 diff_sorted = na_prog_sorted - def_prog_sorted
-symmetric = np.max([np.max(diff_sorted),np.abs(np.min(diff_sorted))])
-plt.imshow(diff_sorted,cmap='seismic',vmin = -symmetric,vmax = symmetric)
-plt.colorbar()
-plt.title("LEVEL Difference between protocols(NA-def)")
-plt.show()
+symmetric = np.max([np.max(diff_sorted), np.abs(np.min(diff_sorted))])
+im = ax.imshow(diff_sorted, cmap='seismic', vmin=-symmetric, vmax=symmetric)
+plt.colorbar(im, cax=cax, orientation="horizontal")
+ax.set_title("LEVEL Difference between protocols(NA-def)")
+# plt.show()
 
 # which base cwl has benefited/got worse
-per_player = np.mean(na_prog_sorted - def_prog_sorted,axis=1)
+per_player = np.mean(na_prog_sorted - def_prog_sorted, axis=1)
 BASE_CWL_sorted = BASE_CWL[sort_ind]
-plt.plot(BASE_CWL_sorted,per_player,color= 'k')
+plt.figure()
+plt.plot(BASE_CWL_sorted, per_player, color='k')
 plt.xlabel("BaseLine CWL")
 plt.ylabel("Mean diff(NA-Def) of levels")
-plt.axhline(linewidth=2, color='r',ls = '--')
-plt.show()
+plt.axhline(linewidth=2, color='r', ls='--')
+# plt.show()
 
 
-def_cwl = cwl[:,:,0]
-na_cwl = cwl[:,:,1]
+def_cwl = m_cwl[:, :, 0]
+na_cwl = m_cwl[:, :, 1]
 
 # heatmap to look at all the players together, sorted by base cwl
-na_cwl_sorted = na_cwl[sort_ind,:]
-def_cwl_sorted = def_cwl[sort_ind,:]
+na_cwl_sorted = na_cwl[sort_ind, :]
+def_cwl_sorted = def_cwl[sort_ind, :]
 images = []
-fig, axs = plt.subplots(1,2)
+fig, axs = plt.subplots(1, 2)
 images.append(axs[0].imshow(na_cwl_sorted))
 axs[0].set_title("workload by NeuroAdaptive")
 images.append(axs[1].imshow(def_cwl_sorted))
@@ -83,28 +100,74 @@ vmax = max(image.get_array().max() for image in images)
 norm = colors.Normalize(vmin=vmin, vmax=vmax)
 for im in images:
     im.set_norm(norm)
-plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9, left = 0.1)
-fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1)
-plt.show()
+plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9, left=0.1)
+fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1, label="Cognitive workload")
+# plt.show()
 
 # look at the difference
 diff_cwl_sorted = na_cwl_sorted - def_cwl_sorted
-plt.imshow(diff_cwl_sorted,cmap='seismic')
-plt.colorbar()
-plt.title("CWL Difference between protocols(NA-def)")
+symmetric = np.max([np.max(diff_cwl_sorted), np.abs(np.min(diff_cwl_sorted))])
+f, (ax, cax) = plt.subplots(2, 1, figsize=(6, 4), gridspec_kw={"height_ratios": [1, 0.05]})
+im = ax.imshow(diff_cwl_sorted, cmap='seismic', vmin=-symmetric, vmax=symmetric)
+plt.colorbar(im, cax=cax, orientation="horizontal")
+ax.set_title("CWL Difference between protocols(NA-def)")
+# plt.show()
+
+# bin players to low,medium,high cwl
+n_groups = 3
+chunk_size = np.floor(NUM_CHARACTERS / n_groups).astype(int)
+bins = np.arange(0, NUM_CHARACTERS, chunk_size)
+bins_cwl = list(map('{:.2f}'.format, na_cwl_sorted[bins, 0]))
+level_data_binned = [
+    (na_prog_sorted[bins[bin]:bins[bin + 1], :] - def_prog_sorted[bins[bin]:bins[bin + 1], :]).mean(axis=0).mean()
+    for bin in range(len(bins) - 1)]
+level_data_binned_std = [
+    sem((na_prog_sorted[bins[bin]:bins[bin + 1], :] - def_prog_sorted[bins[bin]:bins[bin + 1], :]).mean(axis=0))
+    for bin in range(len(bins) - 1)]
+# bar plot by bl cwl
+lbls = ["Low", "Med", "High"]
+fig, ax = plt.subplots()
+x_pos = np.arange(len(level_data_binned))
+ax.bar(x_pos, level_data_binned, yerr=level_data_binned_std, align='center', alpha=0.5, ecolor='black', capsize=10)
+ax.set_ylabel('Mean difference(NA-Def)')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(lbls)
+ax.set_title('Mean diff by base cwl \n cwl edges: ' + str(bins_cwl))
+ax.yaxis.grid(True)
+plt.tight_layout()
+# plt.show()
+
+# line plot
+level_data_binned_ts = [
+    (na_prog_sorted[bins[bin]:bins[bin + 1], :] - def_prog_sorted[bins[bin]:bins[bin + 1], :]).mean(axis=0)
+    for bin in range(len(bins) - 1)]
+level_data_binned_std_ts = [
+    sem((na_prog_sorted[bins[bin]:bins[bin + 1], :] - def_prog_sorted[bins[bin]:bins[bin + 1], :]))
+    for bin in range(len(bins) - 1)]
+fig, axs = plt.subplots()
+for line in range(len(level_data_binned_ts)):
+    axs.plot(level_data_binned_ts[line], label=lbls[line])
+    axs.fill_between(np.arange(level_data_binned_ts[line].shape[0]), level_data_binned_ts[line] -
+                     level_data_binned_std_ts[line], level_data_binned_ts[line] + level_data_binned_std_ts[line], alpha=0.2)
+axs.set_xlabel('Session')
+axs.set_ylabel('Level diff')
+axs.set_title('Level diff by bin and session')
+plt.legend()
 plt.show()
 
-
 # look at data by baseline CWL per player
-base_cwl_sorted = BASE_CWL[sort_ind]
-for ind in range(NUM_CHARACTERS):
+by_player = False
+if by_player:
+    base_cwl_sorted = BASE_CWL[sort_ind]
     plt.style.use("ggplot")
-    plt.plot(def_prog_sorted[ind, :-1], color="black", label="Default")
-    plt.plot(na_prog_sorted[ind, :-1], color="red", label="Neuro-adaptive")
-    plt.title('Compare progress, ' + str(ind) + '. baseline CWL: ' + '{0:.03f}'.format(base_cwl_sorted[ind]))
-    plt.xlabel('Session number')
-    plt.ylabel('level')
-    plt.xticks(np.arange(NUM_SESSIONS))
-    plt.tight_layout()
-    plt.legend()
-    plt.show()
+    for ind in range(NUM_CHARACTERS):
+        plt.figure()
+        plt.plot(def_prog_sorted[ind, :-1], color="black", label="Default")
+        plt.plot(na_prog_sorted[ind, :-1], color="red", label="Neuro-adaptive")
+        plt.title('Compare progress, ' + str(ind) + '. baseline CWL: ' + '{0:.03f}'.format(base_cwl_sorted[ind]))
+        plt.xlabel('Session number')
+        plt.ylabel('level')
+        plt.xticks(np.arange(NUM_SESSIONS))
+        plt.tight_layout()
+        plt.legend()
+        plt.show()
